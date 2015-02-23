652cff8829023a87593009572b6669ba6ae9856f
Graeme Boy <graemeboy@gmail.com>
2015-02-22 19:40:59 -0800

    correct string permutations

--- string-permutations/stringpermutations.js
+++ string-permutations/stringpermutations.js
@@ -1,59 +1,29 @@
 var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     if (line != "") {
-        //findPermutations(line);
-        console.log(findPermutations(line));
+        console.log(combineChars(line).sort().join(","));
     }
 });
 
-function findPermutations(line) {
-    return combineChars(line.split(""));
-}
-
-function findCombs(chars) {
-
-}
-
 function combineChars (chars) {
-
-    // for (var i = 0; i < chars.length; i++) {
-
-    // }
-
-    if (chars.length <= 1) {
-        return chars[0];
+    var permutations = [], words = [], firstChar;
+    if (chars.length === 1) { // base case
+        permutations.push(chars);
+        return permutations;
     }
-
-    console.log("init chars", chars);
-    var nextChars = combineChars(chars.slice(1));
-    console.log("next chars", nextChars);
-    console.log("chars", chars);
-
-    var combination = [];
-
-    for (var j = 0; j < nextChars.length; j++) {
-        for (var i = 0; i < chars.length; i++) {
-            combination.push(chars[i] + nextChars[j]);
+    firstChar = chars[0];
+    chars = chars.substring(1,chars.length);
+    words = combineChars(chars);
+    for (var i = 0; i < words.length; i++) {
+        for (var j = 0; j < words[i].length + 1; j++) {
+            permutations.push(insertCharAt(words[i], firstChar, j));
         }
     }
-    //     for (var j = 0; j < nextChars[0].length; j++) {
-    //         console.log("new comb?", chars[i] + nextChars[j]);
-    //     }
-    return combination;
-    // //console.log("combination", chars[0] + nextChars)
+    return permutations;
+}
 
-    // return chars;
-
-
-    // var combination = [];
-    // for (var j = 0; j < nextChars.length; j++) {
-    //     for (var i = 0; i < chars.length; i++) {
-    //         console.log("new combo?", chars[i] + nextChars[j]);
-    //         combination.push(chars[i] + nextChars[j]);
-    //     }
-        
-    // }
-
-    // return combination;
-
+function insertCharAt(word, charIn, i) {
+    var start = word.substring(0,i);
+    var end = word.substring(i,word.length);
+    return start + charIn + end;
 }
